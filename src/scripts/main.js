d230e554c96fbaac2cd48c4236691489b393cee1
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-10-06 17:26:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+
+wall.style.position = 'relative';
+
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'absolute';
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
