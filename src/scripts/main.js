4553230a026faba334491eac0d860a08133a4dd8
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-10-06 18:31:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,23 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-wall.style.position = 'relative';
+  wall.style.position = 'relative';
 
-const spider = document.querySelector('.spider');
-
-if (spider.complete) {
-  centerSpider();
-} else {
-  spider.onload = () => {
+  if (spider.complete) {
     centerSpider();
-  };
-}
+  } else {
+    spider.addEventListener('load', centerSpider);
+  }
 
-function centerSpider() {
-  const left = (wall.offsetWidth - spider.offsetWidth) / 2;
-  const topOffset = (wall.offsetHeight - spider.offsetHeight) / 2;
+  function centerSpider() {
+    const left = (wall.offsetWidth - spider.offsetWidth) / 2;
+    const topOffset = (wall.offsetHeight - spider.offsetHeight) / 2;
 
-  spider.style.position = 'absolute';
-  spider.style.top = `${topOffset}px`;
-  spider.style.left = `${left}px`;
-}
+    spider.style.position = 'absolute';
+    spider.style.top = `${topOffset}px`;
+    spider.style.left = `${left}px`;
+  }
+});
