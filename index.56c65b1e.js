8981963becfab972a61daa122283a17b1253606e
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-10-06 18:01:41 +0300

    make build

---
+++ index.56c65b1e.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall");t.style.position="relative";var e=document.querySelector(".spider");function o(){var o=(t.offsetWidth-e.offsetWidth)/2,l=(t.offsetHeight-e.offsetHeight)/2;e.style.position="absolute",e.style.top="".concat(l,"px"),e.style.left="".concat(o,"px")}e.complete?o():e.onload=function(){o()};
+//# sourceMappingURL=index.56c65b1e.js.map
